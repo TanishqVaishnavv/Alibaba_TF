04ff509bf97b35c7f2097f235b57d3664a9043ba
TanishqVaishnavv <125270210+TanishqVaishnavv@users.noreply.github.com>
2024-10-02 18:23:34 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  description = "The region to deploy resources"
+  type        = string
+}
+
+variable "owner" {
+  description = "The owner of the resources"
+  type        = string
+}
+
+variable "test_case" {
+  description = "The test case identifier"
+  type        = string
+}
